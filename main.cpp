cc9031b1dba9a56ad0d7814141d2845408326f43
Never Zero <167694532+MasCode256@users.noreply.github.com>
2024-10-25 17:32:24 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -48,12 +48,89 @@ void centralized_print(const string& str) {
     cout << str;
 }
 
+void h_centralized_printLn(const string& str, size_t width) {
+    size_t start_pos = (width > str.length()) ? (width / 2 - str.length() / 2) : 0;
+
+    
+    for(size_t i = 0; i < start_pos; i++) {
+        cout << " ";
+    }
+    cout << str << endl;
+}
+
+void h_centralized_print(const string& str, size_t width) {
+    size_t start_pos = (width > str.length()) ? (width / 2 - str.length() / 2) : 0;
+
+
+    for(size_t i = 0; i < start_pos; i++) {
+        cout << " ";
+    }
+    cout << str;
+}
+
+void print_line(char c) {
+    size_t width = window_width();
+
+    for(size_t i = 0; i < width; i++) {
+        cout << c;
+    }
+}
+
+void h_print_line(char c, size_t width) {
+    for(size_t i = 0; i < width; i++) {
+        cout << c;
+    }
+}
+
+void endl(size_t count) {
+    for (size_t i = 0; i < count; i++)
+    {
+        cout << endl;
+    }
+    
+}
+
 
 int main(int argc, char** argv) {
     clear_console();
     set_color(10);
-    cout << "Window width: " << window_width() << endl;
-    centralized_print("Hello, world!");
+
+    size_t width = window_width();
+    cout << "Window width: " << width << endl;
+
+    Sleep(100);
+
+    const size_t fake_load_str_len = 60;
+    for (size_t i = 0; i < fake_load_str_len; i++)
+    {
+        clear_console();
+
+        h_print_line('_', width);
+        endl(3);
+
+        string load_str = "["; 
+        for (size_t j = 0; j < fake_load_str_len; j++)
+        {
+            if(j <= i) {
+                load_str += "#";
+            } else {
+                load_str += ".";
+            }
+        }
+        
+        load_str += "]";
+
+
+        h_centralized_printLn("Fake loading process...", width);
+        h_centralized_printLn(load_str, width);
+
+        //Sleep(1);
+    }
+    
+
+    h_centralized_print("#vivatC++", window_width());
+    endl(2); h_print_line('-', width);
+    endl(2);
 
     set_color(7);
 }
