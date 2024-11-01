329765dc43a316f22d59076f1c206e4d90ab650c
Never Zero <167694532+MasCode256@users.noreply.github.com>
2024-11-01 12:17:31 +0300

    Add files via upload

--- senior.cpp
+++ senior.cpp
@@ -12,22 +12,27 @@ HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 void clear_console() {
     system("cls");
+    return;
 }
 
 void set_color(int textColor) {
     SetConsoleTextAttribute(hConsole, textColor);
+    return;
+}
+
+void set_background(int bgColor, int textColor = 0) {
+    SetConsoleTextAttribute(hConsole, (bgColor << 4) | textColor);
+    return;
 }
 
 size_t window_width() {
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(hConsole, &csbi);
 
-    // Ширина окна консоли
-    size_t width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    return csbi.srWindow.Right - csbi.srWindow.Left + 1;
 }
 
-void centralized_printLn(const string& str) {
-    size_t width = window_width();
+void centralized_printLn(const string& str, size_t width = window_width()) {
     size_t start_pos = (width > str.length()) ? (width / 2 - str.length() / 2) : 0;
 
     
@@ -35,10 +40,10 @@ void centralized_printLn(const string& str) {
         cout << " ";
     }
     cout << str << endl;
+    return;
 }
 
-void centralized_print(const string& str) {
-    size_t width = window_width();
+void centralized_print(const string& str, size_t width = window_width()) {
     size_t start_pos = (width > str.length()) ? (width / 2 - str.length() / 2) : 0;
 
 
@@ -46,48 +51,23 @@ void centralized_print(const string& str) {
         cout << " ";
     }
     cout << str;
+    return;
 }
 
-void h_centralized_printLn(const string& str, size_t width) {
-    size_t start_pos = (width > str.length()) ? (width / 2 - str.length() / 2) : 0;
-
-    
-    for(size_t i = 0; i < start_pos; i++) {
-        cout << " ";
-    }
-    cout << str << endl;
-}
-
-void h_centralized_print(const string& str, size_t width) {
-    size_t start_pos = (width > str.length()) ? (width / 2 - str.length() / 2) : 0;
-
-
-    for(size_t i = 0; i < start_pos; i++) {
-        cout << " ";
-    }
-    cout << str;
-}
-
-void print_line(char c) {
-    size_t width = window_width();
-
+void print_line(char c, size_t width = window_width()) {
     for(size_t i = 0; i < width; i++) {
         cout << c;
     }
+    return;
 }
 
-void h_print_line(char c, size_t width) {
-    for(size_t i = 0; i < width; i++) {
-        cout << c;
-    }
-}
-
-void endl(size_t count) {
+void endl(size_t count = 1) {
     for (size_t i = 0; i < count; i++)
     {
         cout << endl;
     }
     
+    return;
 }
 
 
@@ -105,7 +85,7 @@ int main(int argc, char** argv) {
     {
         clear_console();
 
-        h_print_line('_', width);
+        print_line('_', width);
         endl(3);
 
         string load_str = "["; 
@@ -121,15 +101,15 @@ int main(int argc, char** argv) {
         load_str += "]";
 
 
-        h_centralized_printLn("Fake loading process...", width);
-        h_centralized_printLn(load_str, width);
+        centralized_printLn("Fake loading process...", width);
+        centralized_printLn(load_str, width);
 
         //Sleep(1);
     }
     
 
-    h_centralized_print("Hello, world!", window_width());
-    endl(2); h_print_line('-', width);
+    centralized_print("#vivatC++", window_width());
+    endl(2); print_line('-', width);
     endl(2);
 
     set_color(7);
